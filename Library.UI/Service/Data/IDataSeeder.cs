8c261f4ce6f18f16091fb054690e072a500f2af4
karaokesound <karaokesound24@gmail.com>
2023-05-18 11:55:55 +0200

    connection to API 2.0

---
+++ Library.UI/Service/Data/IDataSeeder.cs
@@ -0,0 +1,11 @@
+﻿using Library.UI.Service.API.Dto;
+
+namespace Library.UI.Service.Data
+{
+    public interface IDataSeeder
+    {
+        void SeedDataBase();
+
+        void FillDataBase(BookDto[] bookBaseApi);
+    }
+}
